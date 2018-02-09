7a00691e1a0edbe11d0d2a7496f7f4e9d5f0fd2d
Juan A. Manzano <antoniomac_03@hotmail.com>
2018-02-09 05:50:04 -0600

    add new colum to pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ApplicationRecord
+
+  validates :name, presence: true
   validates :photo, presence: true
-  validates :description, presence:true
+  validates :description, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209035248) do
+ActiveRecord::Schema.define(version: 20180209113555) do
 
-  create_table "pins", force: :cascade do |t|
-    t.string "photo"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "pins" because of following StandardError
+#   Unknown type 'String' for column 'name'
 
 end
