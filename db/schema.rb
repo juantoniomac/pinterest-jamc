971243f39affb5c82e05589efddef3c5c5a9a289
Juan A. Manzano <antoniomac_03@hotmail.com>
2018-02-10 16:40:44 -0600

    Cambio estilo pins index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209124802) do
+ActiveRecord::Schema.define(version: 20180210212114) do
 
 # Could not dump table "pins" because of following StandardError
 #   Unknown type 'String' for column 'name'
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180209124802) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
